f06c4da025ed7d6b32d1ae29250ebeec951c0ad5
SingtelPD <71814369+SingtelPD@users.noreply.github.com>
2023-01-12 16:49:53 +0800

    Create deletegamecodes.py

---
+++ deletegamecodes.py
@@ -0,0 +1,60 @@
+#======SUBJECT TO CHANGE===========
+url = "https://fsm.sg.formulasquare.com/fsm_api/wawaji_cms/"
+#==================================
+
+import time
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.headless = True
+
+
+st.title("❌ Delete game codes")
+st.write("Please use the form below to delete the game codes in the system.")
+st.write("Note: You can only delete the X latest game codes")
+
+form = st.form("range_form")
+username = form.text_input("Please enter the CMS username")
+password = form.text_input("Please enter the CMS password")
+num_codes_to_delete = form.text_input("Enter the number of codes to delete")
+submit = form.form_submit_button("Add coupon codes to CMS")
+
+if submit:
+    st.info("Running. Please do NOT click on the button again.")
+
+    #convert start_num and end_num to integer
+    num_codes_to_delete = int(num_codes_to_delete)+1
+
+    web = webdriver.Chrome(options=options)
+    web.get(url)
+    time.sleep(3)
+
+    #Login
+    username_field = web.find_element("xpath", '/html/body/div/div/main/div/div/div/div/form/div[1]/input')
+    username_field.send_keys(username)
+    password_field = web.find_element("xpath", '/html/body/div/div/main/div/div/div/div/form/div[3]/input')
+    password_field.send_keys(password)
+    submit_login_button = web.find_element("xpath", '/html/body/div/div/main/div/div/div/div/form/div[5]/input')
+    submit_login_button.click()
+    time.sleep(2)
+
+    #navigate to coupon codes page
+    coupon_codes = web.find_element("xpath", '/html/body/div/div/main/div/div[1]/button[3]')
+    coupon_codes.click()
+    time.sleep(1)
+
+    #enter coupon codes within range
+    for i in range (num_codes_to_delete):
+        delete_button = web.find_element("xpath", '/html/body/div/div/main/div/div[2]/div/table/tbody/tr[1]/td[1]/input')
+        delete_button.click()
+        time.sleep(1)
+        wait.until(expected_conditions.alert_is_present())
+        alert = web.switch_to.alert.accept()
+        time.sleep(1)
+        st.write(f"Coupon code {i} has been deleted from the CMS system.")
+
+    num_codes_to_delete = num_codes_to_delete-1
+    st.success(f"{num_codes_to delete} codes have been added to the CMS system.")
